05f2b8462fadf01787d7b759098c13e6336a3953
AlfredoLopez <Lopezcervantesjosealfredo@gmail.com>
2022-11-28 22:29:48 -0600

    agg

--- Parcial3/Grid/ConexiondeBD.php
+++ Parcial3/Grid/ConexiondeBD.php
@@ -1,18 +1,13 @@
-<?php
-
+<?php 
 $servidor = "localhost";
 $basededatos = "supermercado";
 $usuario = "root";
-$paswoord = "";
-$con = mysqli_connect($servidor,$usuario,$paswoord,$basededatos) or die ('No se pudo conectar'.mysql_error());
-$consulta = "SELECT * FROM articulos";
-
+$pasword = "";
+$con = mysqli_connect($servidor,$usuario,$password,$basededatos) or die ("No se pudo conectar a localhost");
+$consulta = "select Codigo,Nombre,Ciudad from sucursales";
 $registros = mysqli_query($con,$consulta) or die ("Problemas en el select");
 
-$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
-
+$result= mysqli_fetch_all($registros,MYSQLI_ASSOC);
 mysqli_close($con);
-
 echo json_encode($result);
-
 ?>
