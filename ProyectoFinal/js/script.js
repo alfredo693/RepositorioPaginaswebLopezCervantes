0ca61262e70657e4f2ff765d84fa3ed385949302
AlfredoLopez <Lopezcervantesjosealfredo@gmail.com>
2022-11-30 07:25:02 -0600

    agg

--- ProyectoFinal/js/script.js
+++ ProyectoFinal/js/script.js
@@ -1,83 +1,28 @@
 $(document).ready(function() {
 
-//Consulta base de datos // 
-$('#btnConsultarBD').click(function() {
-    $('#modal1').modal();
-    $('#modal1').on('hidden.bs.modal',function(e){
-        let paridCte=$('#numregistro').val();
-        $.post('./php/conectar.php',{par1:paridCte},function(data){
-            refrescar(data);
-        },'json');
-    })
-});
-
-$('#btnBorrarBD').click(function() {
-        let paridCte=$('#numregistro').val();
-        $.post('Borrar.php',{par1:paridCte},function(data){
-            refrescar(data);
-        },'json');
-
-        Swal.fire(
-            'Accion' , 
-             'Usuario Eliminado' , 
-             'success', 
-            );
-    
-});
-
-$('#btnInsertarBD').click(function() {
-    let titulo = $('#titulo').val();
-    let fechadeingreso =  $('#fechadeingreso').val();
-    let editorial = $('#editorial').val();
-    let numerodepaginas = $('#numero de paginas').val();
-    let autor = $('#autor').val();
-    let  tipo = $('#tipo').val();
-    let idioma = $('#idioma').val();
-    let precio =$('#precio').val();
-    $.post('./php/insertar.php',{ti:titulo,fe:fechadeingreso,edit:editorial,numpag:numerodepaginas,aut:autor,tip:Tipo,idioma:idioma,pr:precio
+    //Consulta base de datos // 
+    $('#btnConsultarBD').click(function() {
+        $('#modal1').modal();
+        $('#modal1').on('hidden.bs.modal',function(e){
+            let paridCte=$('#idConsulta').val();
+            $.post('./php/conectar.php',{par1:paridCte},function(data){
+                refrescar(data);
+            },'json');
+        })
     });
-    Swal.fire(
-        'Accion' , 
-         'Usuario insertado' , 
-         'success', 
-        );
-});
 
-function refrescar(objeto){
-    console.log(objeto);
-    $('#numregistro').val(objeto.numregistro);
-    $('#titulo').val(objeto.titulo);
-    $('#fechadeingreso').val(objeto.fechadeingreso);
-    $('#editorial').val(objeto.editorial);
-    $('#numerodepaginas').val(objeto.numerodepaginas);
-    $('#autor').val(objeto.autor);
-    $('#tipo').val(objeto.tipo);
-    $('#idioma').val(objeto.idioma);
-    $('#precio').val(objeto.precio);
-}
-
-$('#btnModificarBD').click(function() {
-    let paridCte=$('#numregistro').val();
-    let titulo = $('#titulo').val();
-    let fechadeingreso =  $('#fechadeingreso').val();
-    let editorial = $('#editorial').val();
-    let numerodepaginas = $('#numerodepaginas').val();
-    let autor = $('#autor').val();
-    let tipo = $('#tipo').val();
-    let idioma = $('#idioma').val();
-    let precio =$('#precio').val();
-    $.post('./php/Modificar.php',{par1:paridCte,nom:titulo,Fe:fechadeingreso,Nacio:editorial,Estat:numerodepaginas,Equi:autor,Gen:tipo,Les:idioma,Pos:precio
-    });
-    Swal.fire(
-        'Accion' , 
-         'Usuario modificado' , 
-         'success', 
-        );
-});
-
-
-
-
+    function refrescar(objeto){
+        console.log(objeto);
+        $('#numregistro').val(objeto.numregistro);
+        $('#titulo').val(objeto.titulo);
+        $('#fechadeingreso').val(objeto.fechadeingreso);
+        $('#editorial').val(objeto.editorial);
+        $('#numerodepaginas').val(objeto.numerodepaginas);
+        $('#autor').val(objeto.autor);
+        $('#tipo').val(objeto.tipo);
+        $('#idioma').val(objeto.idioma);
+        $('#precio').val(objeto.precio);
+    }
 });
 
 
--- ProyectoFinal/php/conectar.php
+++ ProyectoFinal/php/conectar.php
@@ -1,6 +1,7 @@
 <?php
-    $paridCte= $_POST['par1'];
-    $hostname = 'localhost' ;
+
+   $paridCte=$_POST['par1'];
+   $hostname = 'localhost' ;
     $database = 'bibloteca' ;
     $username = 'root' ;
     $password = '' ;
@@ -14,17 +15,16 @@
   }
 
   try{
-    $sql = "SELECT * FROM libros WHERE numregistro=".$paridCte;
+    $sql = "select * from libros where numregistro=".$paridCte;
     $query = $con->prepare($sql);
     $query->execute();
     $result = $query->fetch(PDO::FETCH_ASSOC);
     $query->closeCursor();
-}
-catch(PDOException $e) {
+}catch(PDOException $e) {
     echo "Error de consulta a la base de datos";
     echo $e->getMessage();
 }
 
 echo json_encode($result);
 
-?>
+ ?>
