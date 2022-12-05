2d0a25dd2849d23b49b2daef46895a7fb1f29a75
AlfredoLopez <Lopezcervantesjosealfredo@gmail.com>
2022-12-04 19:44:59 -0600

    agg

---
+++ Parcial3/Showpdf/Mensaje.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=$_POST['nombre'];
+require('./FPDF/fpdf.php');
+
+$pdf=nuevoFPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Jose Alfredo Lopez Cervantes'. $nombre);
+$pdf->Salida();
+?>
